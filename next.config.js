fc87d7015028e0aa55591cc87cdb7543903b9172
Suman <sumansinghsaini9234@gmail.com>
2025-04-28 12:13:53 +0200

    Final commits

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const nextConfig = {
+    reactStrictMode: true,
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  module.exports = nextConfig;
